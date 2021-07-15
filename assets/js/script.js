dec1384a006b24e69ebe181abe129b7bdfe35f01
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-07-15 19:08:25 -0400

    results display as cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,58 @@
 var searchButton = $("#search-btn");
 //var searchInput = $("#searchBar").val();
 //var resultsText = $("#results-list");
-var resultsSec = $("#results-section") 
+var resultsSec = $("#results-section")
 
 function getApi(event) {
   event.preventDefault()
   var searchInput = $("#search-bar").val();
   console.log(searchInput)
-    var requestUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchInput + "&key=AIzaSyA3C3fX17i43ey6iVthUwijF1A1MySz0lU";
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (response) {
+  var requestUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchInput + "&key=AIzaSyA3C3fX17i43ey6iVthUwijF1A1MySz0lU";
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(searchInput);
+      //console.log(requestUrl)
+      console.log(response);
+      //console.log(response.kind);
+      //console.log(response.totalItems);
+      console.log(response.items);
+      for (var i = 0; i < response.items.length; i++) {
+        var bookCard = document.createElement('div');
+        bookCard.setAttribute('class', 'card');
+        bookCard.setAttribute('style', 'width: 150px')
+
+        var resultsText = document.createElement('li');
+        resultsText.textContent = response.items[i].volumeInfo.title;
+
+        var linkText = document.createElement('a');
+        linkText.textContent = "  Click Here";
+        linkText.href = response.items[i].volumeInfo.infoLink;
+        resultsText.append(linkText);
+
+        //console.log(response.items[i].volumeInfo);
+        console.log(response.items[i].volumeInfo.imageLinks.thumbnail);
+        var thumbImg = document.createElement('img');
+        thumbImg.src = response.items[i].volumeInfo.imageLinks.thumbnail;
+        bookCard.append(thumbImg);
+
+        bookCard.append(resultsText);
+        resultsSec.append(bookCard);
+
+        console.log(response.items[i].volumeInfo.infoLink);
+        //console.log(response.items[i].volumeInfo.industryIdentifiers)
+      }
+      if (searchInput != "") {
         console.log(searchInput);
-        //console.log(requestUrl)
-        console.log(response);
-        //console.log(response.kind);
-        //console.log(response.totalItems);
-        console.log(response.items);
-        for (var i = 0; i < response.items.length; i++) {
-          var resultsText = document.createElement('li');
-          resultsText.textContent = response.items[i].volumeInfo.title
-          resultsSec.append(resultsText)
-
-          var linkText = document.createElement('a');
-          linkText.textContent = "  Click Here"
-          linkText.href = response.items[i].volumeInfo.infoLink
-          resultsText.append(linkText)
-
-          //console.log(response.items[i].volumeInfo);
-          console.log(response.items[i].volumeInfo.imageLinks.thumbnail);
-          var thumbImg = document.createElement('img');
-          thumbImg.src = response.items[i].volumeInfo.imageLinks.thumbnail
-          resultsSec.append(thumbImg)
-
-          console.log(response.items[i].volumeInfo.infoLink);
-          //console.log(response.items[i].volumeInfo.industryIdentifiers)
-        }
-        if (searchInput != "") {
-          console.log(searchInput);
-        } else {
-          (function(response) {
-            console.log(searchInput)
-          })
-        }
-      });
-  }
+      } else {
+        (function (response) {
+          console.log(searchInput)
+        })
+      }
+    });
+}
 searchButton.on('click', getApi);
 
 
