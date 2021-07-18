ffbecf6de7188f0aa9da0d1a9085ebd541bb992d
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-07-18 16:54:11 -0400

    favoriting an item populates a book card in the favorites section

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,7 @@ function getFavorites() {
 }
 getFavorites();
 
+// || Clear search results
 var clearSearch = $("#clear")
 function clear(event) {
   event.preventDefault()
@@ -113,12 +114,55 @@ getNytApi(requestUrlNyt);
 
 function saveBook() {
 
-  console.log($(this).siblings().eq(2).prevObject[1].innerHTML);
-  storageArr = storageArr.concat($(this).siblings().eq(2).prevObject[1].innerHTML);
+  var isbnRetrieval = $(this).siblings().eq(2).prevObject[1].innerHTML;
+  console.log(isbnRetrieval);
+  storageArr = storageArr.concat(isbnRetrieval);
   console.log(storageArr);
   localStorage.setItem('book', JSON.stringify(storageArr));
   console.log(localStorage.getItem('book'));
   console.log('click');
+
+  var requestUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbnRetrieval}`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var bookCard = document.createElement('li');
+      bookCard.setAttribute('class', 'card cell small-2 result-card');
+
+      var linkBook = document.createElement('a');
+      linkBook.href = data.items[0].volumeInfo.infoLink;
+      linkBook.setAttribute('target', '_blank');
+      bookCard.append(linkBook);
+
+      //console.log(response.items[i].volumeInfo);
+      console.log(data.items[0].volumeInfo.imageLinks.thumbnail);
+      var thumbImg = document.createElement('img');
+      thumbImg.setAttribute('alt', `${data.items[0].volumeInfo.title}`)
+      thumbImg.src = data.items[0].volumeInfo.imageLinks.thumbnail;
+      linkBook.append(thumbImg);
+
+      var favoriteEl = document.createElement('span');
+      favoriteEl.setAttribute('class', 'label warning');
+      favoriteEl.textContent = 'Favorite Me';
+      bookCard.append(favoriteEl);
+
+      var isbnNumber = document.createElement('span');
+      isbnNumber.textContent = data.items[0].volumeInfo.industryIdentifiers[0].identifier;
+      // console.log(response.items[i].volumeInfo.industryIdentifiers[0].identifier);
+      // console.log(isbnNumber.textContent);
+      isbnNumber.setAttribute('style', 'display: none');
+      bookCard.append(isbnNumber);
+
+      favoriteMenu.append(bookCard);
+
+      // console.log(response.items[i].volumeInfo.infoLink);
+      // console.log(resultCard);
+
+    })
 }
 
 resultsList.on('click', '.label', saveBook);
