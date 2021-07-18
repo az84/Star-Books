5fc47ea7481aaa91414b6a06e86d38914eea9e9a
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-07-18 17:06:31 -0400

    favorites menu is populated with local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,45 @@ var favoriteMenu = $('.favorite-menu');
 var storageArr = [];
 
 function getFavorites() {
-  // storageArr = storageArr.concat(JSON.parse(localStorage.getItem('book')));
-  // console.log(storageArr);
+  storageArr = storageArr.concat(JSON.parse(localStorage.getItem('book')));
+  console.log(storageArr);
 
-  // var testItem = $('<li>');
-  // testItem.text('THIS IS A TEST');
-  // favoriteMenu.append(testItem);
+  for (var i = 0; i < storageArr.length; i++) {
+    var requestUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${storageArr[i]}&key=AIzaSyA3C3fX17i43ey6iVthUwijF1A1MySz0lU`;
 
-  // for (var i = 0; i < storageArr.length; i++) {
-  //   favoriteMenu.append(storageArr[i]);
-  // }
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        var bookCard = document.createElement('li');
+        bookCard.setAttribute('class', 'card cell small-2 result-card');
+
+        var linkBook = document.createElement('a');
+        linkBook.href = data.items[0].volumeInfo.infoLink;
+        linkBook.setAttribute('target', '_blank');
+        bookCard.append(linkBook);
+
+        console.log(data.items[0].volumeInfo.imageLinks.thumbnail);
+        var thumbImg = document.createElement('img');
+        thumbImg.setAttribute('alt', `${data.items[0].volumeInfo.title}`)
+        thumbImg.src = data.items[0].volumeInfo.imageLinks.thumbnail;
+        linkBook.append(thumbImg);
+
+        var favoriteEl = document.createElement('span');
+        favoriteEl.setAttribute('class', 'label warning');
+        favoriteEl.textContent = 'Favorite Me';
+        bookCard.append(favoriteEl);
+
+        var isbnNumber = document.createElement('span');
+        isbnNumber.textContent = data.items[0].volumeInfo.industryIdentifiers[0].identifier;
+        isbnNumber.setAttribute('style', 'display: none');
+        bookCard.append(isbnNumber);
+
+        favoriteMenu.append(bookCard);
+      })
+  }
 }
 getFavorites();
 
@@ -122,7 +151,7 @@ function saveBook() {
   console.log(localStorage.getItem('book'));
   console.log('click');
 
-  var requestUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbnRetrieval}`;
+  var requestUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbnRetrieval}&key=AIzaSyA3C3fX17i43ey6iVthUwijF1A1MySz0lU`;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -138,7 +167,6 @@ function saveBook() {
       linkBook.setAttribute('target', '_blank');
       bookCard.append(linkBook);
 
-      //console.log(response.items[i].volumeInfo);
       console.log(data.items[0].volumeInfo.imageLinks.thumbnail);
       var thumbImg = document.createElement('img');
       thumbImg.setAttribute('alt', `${data.items[0].volumeInfo.title}`)
@@ -152,16 +180,10 @@ function saveBook() {
 
       var isbnNumber = document.createElement('span');
       isbnNumber.textContent = data.items[0].volumeInfo.industryIdentifiers[0].identifier;
-      // console.log(response.items[i].volumeInfo.industryIdentifiers[0].identifier);
-      // console.log(isbnNumber.textContent);
       isbnNumber.setAttribute('style', 'display: none');
       bookCard.append(isbnNumber);
 
       favoriteMenu.append(bookCard);
-
-      // console.log(response.items[i].volumeInfo.infoLink);
-      // console.log(resultCard);
-
     })
 }
 
